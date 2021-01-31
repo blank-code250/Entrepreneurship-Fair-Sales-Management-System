ebff7b815a44f67b5be1d46e6a959d54b59f0f58
Lucas Oliveira Lima <emailexterno4@gmail.com>
2021-01-31 16:02:10 -0300

    Add support to fx:root  of home view

--- src/br/com/efsms/Main.java
+++ src/br/com/efsms/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import javafx.fxml.FXMLLoader;
@@ -14,7 +15,10 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
         primaryStage.setTitle("Sistema de Gerenciamento de Vendas para a Feira de Empreendedorismo");
-        Parent root = FXMLLoader.load(getClass().getResource("view/fxml/home.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("view/fxml/home.fxml"));
+        Pane root = new Pane();
+        loader.setRoot(root);
+        loader.load();
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
