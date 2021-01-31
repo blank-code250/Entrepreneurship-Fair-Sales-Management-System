e662b31c95d16b69eec86fc6b7ff8714ed302a17
Lucas Oliveira Lima <emailexterno4@gmail.com>
2021-01-31 15:20:58 -0300

    Add add product view logic skeleton

---
+++ src/br/com/efsms/view/addProduct.java
@@ -0,0 +1,40 @@
+package br.com.efsms.view;
+
+import javafx.event.ActionEvent;
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import javafx.scene.layout.Pane;
+
+public class addProduct {
+
+    @FXML
+    private Pane root;
+
+    @FXML
+    private TextField name;
+
+    @FXML
+    private TextField price;
+
+    @FXML
+    private Button addProduct;
+
+    @FXML
+    private Button backToHome;
+
+    @FXML
+    void addProduct(ActionEvent event) {
+
+    }
+
+    @FXML
+    void renderHome(ActionEvent event) {
+
+    }
+
+}
+
